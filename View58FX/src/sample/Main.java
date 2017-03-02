fb416d82f18e660b2e57787735cf91abb4a313b3
ZHONG SIFEN <zhongsifen@gmail.com>
2017-03-02 21:20:41 +0800

    VIew58FX: init

--- View58FX/src/sample/Main.java
+++ View58FX/src/sample/Main.java
@@ -5,6 +5,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
@@ -13,7 +14,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
-        Parent root = loader.load(); // FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root;
+        root = loader.load();
         primaryStage.setTitle("View58");
         primaryStage.setScene(new Scene(root, 1280, 960));
         primaryStage.show();
