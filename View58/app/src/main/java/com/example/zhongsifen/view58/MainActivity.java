e11960ca41f956dd19b499adbf16c9ca8ea8cafd
ZHONG SIFEN <zhongsifen@gmail.com>
2017-04-05 21:26:16 +0800

    View58

--- View58/app/src/main/java/com/example/zhongsifen/view58/MainActivity.java
+++ View58/app/src/main/java/com/example/zhongsifen/view58/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.zhongsifen.view58;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 import java.io.File;
 import java.io.InputStream;
@@ -58,6 +62,23 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri imageUri = data.getData();
+            Bitmap bitmap;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                ImageView imageView = (ImageView)findViewById(R.id.imageView);
+                imageView.setImageBitmap(bitmap);
+            } catch (Exception e) {}
+        }
+
+
     }
 
 }
